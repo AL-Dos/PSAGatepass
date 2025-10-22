27e41ad775fa1e125acfa9f38da1a416cfc67af9
AL-Dos <barbarianclaw1@gmail.com>
2025-10-22 11:28:05 +0800

    feat: added folders for front component and pages

---
+++ frontend/src/app/Pages/audit-page/audit-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-page',
+  imports: [],
+  templateUrl: './audit-page.html',
+  styleUrl: './audit-page.css'
+})
+export class AuditPage {
+
+}
