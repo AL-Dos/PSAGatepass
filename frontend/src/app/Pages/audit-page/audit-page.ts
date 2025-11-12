18b2e0cd7859160de807f25a4c008ecb95fe037e
Admin <omictinaldous@gmail.com>
2025-11-12 15:43:07 +0800

    feat: database log page

--- frontend/src/app/Pages/audit-page/audit-page.ts
+++ frontend/src/app/Pages/audit-page/audit-page.ts
@@ -1,8 +1,19 @@
+import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-audit-page',
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule,
+            MatInputModule, MatButtonModule, MatIconModule, MatDatepickerModule,
+            MatNativeDateModule],
   templateUrl: './audit-page.html',
   styleUrl: './audit-page.css'
 })
