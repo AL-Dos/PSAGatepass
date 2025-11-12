ea5b9ba3359b326247609102c9d82c0d35efd8c6
Admin <omictinaldous@gmail.com>
2025-11-12 15:20:15 +0800

    feat: non-functional front page

--- frontend/src/app/Components/audit-form/audit-form.ts
+++ frontend/src/app/Components/audit-form/audit-form.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,10 +11,11 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-audit-form',
   imports: [FormsModule, CommonModule, MatCardModule, MatIconModule, MatButtonModule,
-            MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatDialogModule],
+            MatInputModule, MatFormFieldModule, MatDialogModule],
   templateUrl: './audit-form.html',
   styleUrl: './audit-form.css'
 })
 export class AuditForm {
+description: any;
 
 }
