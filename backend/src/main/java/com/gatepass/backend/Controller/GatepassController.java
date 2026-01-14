a758b9ccd403c06c690d9b1ae7580e373a00f6b7
Admin <omictinaldous@gmail.com>
2026-01-14 12:04:40 +0800

    fix: delete button icon not centered

--- backend/src/main/java/com/gatepass/backend/Controller/GatepassController.java
+++ backend/src/main/java/com/gatepass/backend/Controller/GatepassController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/api")
 public class GatepassController {
